1e694a66f9e533911f386eeaecb4c69b7996afcd
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-02-27 22:33:42 +0100

    add abstract class

---
+++ src/exercise/animal/Animal.java
@@ -0,0 +1,19 @@
+package exercise.animal;
+
+public abstract class Animal {
+    private boolean mammal;
+    
+    public Animal(boolean mammal){
+        this.mammal = mammal;
+    }
+
+    public void dormi(){
+        System.out.println("zZzZz...");
+    };
+
+    //(mostra a video il verso fatto dall'animale specifico)
+    public abstract void verso();
+
+    //(mostra a video quello che mangia : erba, carne, ...?
+    public abstract void mangia();
+}
